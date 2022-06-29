c216e24b72978f61b804fc66779da7faa2f4be1c
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-29 07:39:41 -0500

    Code seems to be working but tests failing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+  target = document.getElementById("text");
+  target.textContent = "This is really cool!";
+});
+
+// Code seems to be working in the browser, but tests are still failing.
+// Test gets unchanged text as a result but the text is changed in the browser.
