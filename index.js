f7c6b77420b123be248406ab3a8d23f2687c6551
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-29 09:16:33 -0500

    commit tree 6cf48a20dd785e05c04fd63d3aff30564a47d389

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-  target = document.getElementById("text");
-  target.textContent = "This is really cool!";
+  document.getElementById("text").textContent = "This is really cool!";
 });
-
-// Code seems to be working in the browser, but tests are still failing.
-// Test gets unchanged text as a result but the text is changed in the browser.
